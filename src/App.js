3f63104c769a11b0bd0a91cb5ad7e3a6bc54c27c
FraanSnk <franconicolas554@gmail.com>
2022-04-23 18:03:31 -0300

    implementacion de navbar y footer

--- src/App.js
+++ src/App.js
@@ -1,78 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/NavBar';
+import Footer from './components/Footer';
+import Marcas from './components/container/ItemListContainer';
 
 function App() {
   return (
 <>
-    <header>
-      <div class="navbar bg-base-100">
-        <div class="flex-1">
-          <a class="btn btn-ghost normal-case text-xl">Las Delicias</a>
-        </div>
-        <div class="form-control">
-          <div class="input-group">
-            <input type="text" placeholder="Buscar…" class="input input-bordered"/>
-            <button class="btn btn-square">
-              <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
-            </button>
-          </div>
-        </div>
-        <div class="flex-none">
-          <div class="dropdown dropdown-end">
-            <label tabindex="0" class="btn btn-ghost btn-circle">
-              <div class="indicator">
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" /></svg>
-                <span class="badge badge-sm indicator-item">8</span>
-              </div>
-            </label>
-            <div tabindex="0" class="mt-3 card card-compact dropdown-content w-52 bg-base-100 shadow">
-              <div class="card-body">
-                <span class="font-bold text-lg">8 Productos</span>
-                <span class="text-info">Subtotal: $999</span>
-                <div class="card-actions">
-                  <button class="btn btn-primary btn-block">Ver carrito</button>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="dropdown dropdown-end">
-            <label tabindex="0" class="btn btn-ghost btn-circle avatar">
-              <div class="w-10 rounded-full">
-                <img src="https://api.lorem.space/image/face?hash=33791" />
-              </div>
-            </label>
-            <ul tabindex="0" class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-              <li>
-                <a class="justify-between">
-                  Perfil
-                  <span class="badge">Nuevo!</span>
-                </a>
-              </li>
-              <li><a>Configuracion</a></li>
-              <li><a>Cerrar sesion</a></li>
-            </ul>
-          </div>
-        </div>
-      </div>
-    </header>
-
-    <footer class="footer footer-center p-10 bg-base-200 text-base-content rounded">
-      <div class="grid grid-flow-col gap-4">
-        <a class="link link-hover">Sobre Nosotros</a> 
-        <a class="link link-hover">Contactactanos</a> 
-        <a class="link link-hover">Trabaja con nosotros</a>
-      </div> 
-      <div>
-        <div class="grid grid-flow-col gap-4">
-          <a><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" class="fill-current"><path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"></path></svg></a> 
-          <a><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" class="fill-current"><path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"></path></svg></a> 
-          <a><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" class="fill-current"><path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path></svg></a>
-        </div>
-      </div> 
-      <div>
-        <p>Copyright © 2022 - All right reserved by ACME Industries Ltd</p>
-      </div>
-    </footer>
+    <NavBar/>
+    <Marcas/>
+    <Footer/>
 </>    
   );
 }
